63b16bf2495d3c4eb04510a5d725952dff4323e6
jxldjsn <tianyaguniao@outlook.com>
2018-09-25 18:32:35 +0800

    20180925提交测试

--- test/basic/test/GameTest.java
+++ test/basic/test/GameTest.java
@@ -6,7 +6,9 @@ public class GameTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Long aLong = null;
-		if (null == aLong) {
+		if (null == aLong) 
+			
+			System.out.println("GameTest");
 			
 		}
 	}
